7cb8ea57e78611bcc018fad083a7476c57fef967
rainbowmeh <rainbowmeh@gmail.com>
2020-05-03 22:47:16 +0700

    Laporan filter and jsGrid Done, not yet check

--- laporan/index.php
+++ laporan/index.php
@@ -39,55 +39,58 @@
 
     <!-- Main content -->
     <section class="content">
-      <div class="row">
-        <div class="col-md-4">
-          <div class="form-group">
-            <label>Kebun</label>
-            <select class="form-control select2bs4" id="kebunSelect" style="width: 100%;" data-placeholder="Kebun">
-              <option selected="selected" value="" disabled></option>
-              <option value="KLB">KLB</option>
-              <option value="KLM">KLM</option>
-              <option value="KLO">KLO</option>
-              <option value="KLP">KLP</option>
-              <option value="KLT">KLT</option>
-              <option value="KLU">KLU</option>
-            </select>
-          </div>
-          <!-- /.form-group -->
-          <div class="form-group">
-            <label>KUD</label>
-            <select class="form-control select2bs4"id="kudSelect" style="width: 100%;" data-placeholder="KUD">
-              <option selected="selected" value="" disabled></option>
-            </select>
-          </div>
-          <!-- /.form-group -->
-          <div class="form-group">
-            <label>KT</label>
-            <select class="form-control select2bs4" id="ktSelect" style="width: 100%;" data-placeholder="KT">
-              <option selected="selected" value="" disabled></option>
-            </select>
-          </div>
-          <!-- /.form-group -->
-        </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-4">
-          <div class="form-group">
-            <label>Tanggal kunjungan</label>
-            <input type="Text" class="form-control" placeholder="Tanggal kunjungan" id="tanggalPicker">
-            <span class="text-sm">*Note: Tanggal yang tidak ditandai berarti kosong</span>
-          </div>
-          <!-- /.form-group -->
-          <div class="form-group">
-            <label>Keterangan</label>
-            <textarea class="form-control" rows="4" placeholder="Keterangan"></textarea>
-          </div>
-          <!-- /.form-group -->
-        </div>
-      </div>
       <div class="card">
         <!-- /.card-header -->
         <div class="card-body">
-<!--          <div id="jsGrid1"></div>-->
+          <div class="row">
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Kebun</label>
+                <select class="form-control select2bs4" id="kebunSelect" style="width: 100%;" data-placeholder="Kebun">
+                  <option selected="selected" value="" disabled></option>
+                  <option value="KLB">KLB</option>
+                  <option value="KLM">KLM</option>
+                  <option value="KLO">KLO</option>
+                  <option value="KLP">KLP</option>
+                  <option value="KLT">KLT</option>
+                  <option value="KLU">KLU</option>
+                </select>
+              </div>
+              <!-- /.form-group -->
+              <div class="form-group">
+                <label>KUD</label>
+                <i class="fas fa-circle-notch fa-spin" id="kudSpinner" hidden></i>
+                <select class="form-control select2bs4"id="kudSelect" style="width: 100%;" data-placeholder="KUD">
+                  <option selected="selected" value="" disabled></option>
+                </select>
+              </div>
+              <!-- /.form-group -->
+              <div class="form-group">
+                <label>KT</label>
+                <i class="fas fa-circle-notch fa-spin" id="ktSpinner" hidden></i>
+                <select class="form-control select2bs4" id="ktSelect" style="width: 100%;" data-placeholder="KT">
+                  <option selected="selected" value="" disabled></option>
+                </select>
+              </div>
+              <!-- /.form-group -->
+            </div>
+            <div class="col-md-2"></div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Tanggal kunjungan</label>
+                <i class="fas fa-circle-notch fa-spin" id="tanggalSpinner" hidden></i>
+                <input type="Text" class="form-control" placeholder="Tanggal kunjungan" id="tanggalPicker">
+                <span class="text-sm">*Note: Tanggal yang tidak ditandai berarti kosong</span>
+              </div>
+              <!-- /.form-group -->
+              <div class="form-group">
+                <label>Keterangan</label>
+                <textarea class="form-control" rows="4" placeholder="Keterangan"></textarea>
+              </div>
+              <!-- /.form-group -->
+            </div>
+          </div>
+          <div id="jsGrid1"></div>
         </div>
         <!-- /.card-body -->
       </div>
@@ -129,6 +132,7 @@
   var selectedOptionKt;
   var availableDates = [];
   var selectedDate;
+  var data = [];
 
   $("#tanggalPicker").datepicker({
     changeMonth: true,
@@ -149,11 +153,21 @@
   $('#kebunSelect').on('change', function() {
     console.log("kebun onChange " + this.value);
     selectedOptionKebun = this.value;
+    $('#tanggalSpinner').removeAttr('hidden');
+    $('#ktSpinner').removeAttr('hidden');
+    $('#kudSpinner').removeAttr('hidden');
     db.collection("report")
       .where("kebun", "==", selectedOptionKebun)
       .get().then((querySnapshot) => {
         $('#kudSelect').empty();
         $('#ktSelect').empty();
+        $("#tanggalPicker").datepicker("refresh");
+        $("#tanggalPicker").datepicker("setDate", null);
+        availableDates = [];
+        if (data.length) {
+          data = [];
+          load();
+        }
         optionList = '';
         optionList += '<option value="" selected="selected" disabled></option>';
         index = [];
@@ -163,6 +177,9 @@
             optionList += '<option value="' + doc.data().kud + '">' + doc.data().kud + '</option>';
           }
         });
+        $('#kudSpinner').attr('hidden', '');
+        $('#ktSpinner').attr('hidden', '');
+        $('#tanggalSpinner').attr('hidden', '');
         $('#kudSelect').append(optionList);
       })
   })
@@ -170,11 +187,20 @@
   $('#kudSelect').on('change', function() {
     console.log("kud onChange " + this.value);
     selectedOptionKud = this.value;
+    $('#tanggalSpinner').removeAttr('hidden');
+    $('#ktSpinner').removeAttr('hidden');
     db.collection("report")
       .where("kebun", "==", selectedOptionKebun)
       .where("kud", "==", selectedOptionKud)
       .get().then((querySnapshot) => {
         $('#ktSelect').empty();
+        $("#tanggalPicker").datepicker("refresh");
+        $("#tanggalPicker").datepicker("setDate", null);
+        availableDates = [];
+        if (data.length) {
+          data = [];
+          load();
+        }
         optionList = '';
         optionList += '<option value="" selected="selected" disabled></option>';
         index = [];
@@ -184,6 +210,8 @@
             optionList += '<option value="' + doc.data().kt + '">' + doc.data().kt + '</option>';
           }
         });
+        $('#ktSpinner').attr('hidden', '');
+        $('#tanggalSpinner').attr('hidden', '');
         $('#ktSelect').append(optionList);
       })
   })
@@ -191,17 +219,25 @@
   $('#ktSelect').on('change', function() {
     console.log("kt onChange " + this.value);
     selectedOptionKt = this.value;
+    $('#tanggalSpinner').removeAttr('hidden');
     db.collection("report")
       .where("kebun", "==", selectedOptionKebun)
       .where("kud", "==", selectedOptionKud)
       .where("kt", "==", selectedOptionKt)
       .get().then((querySnapshot) => {
+        $("#tanggalPicker").datepicker("refresh");
+        $("#tanggalPicker").datepicker("setDate", null);
         availableDates = [];
+        if (data.length) {
+          data = [];
+          load();
+        }
         querySnapshot.forEach((doc) => {
           console.log(doc.data().tanggal);
           availableDates.push((doc.data().tanggal.seconds * 1000));
           console.log(availableDates);
         });
+        $('#tanggalSpinner').attr('hidden', '');
       })
   })
 
@@ -213,29 +249,19 @@
       .where("kud", "==", selectedOptionKud)
       .where("kt", "==", selectedOptionKt)
       .where("tanggal", "==", selectedDate)
-      .get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-      });
-    })
+      .onSnapshot((querySnapshot) => {
+        data = [];
+        querySnapshot.forEach((doc) => {
+          const tempData = doc.data();
+          tempData['keys'] = doc.id;
+          data.push(tempData);
+          console.log(doc.data());
+        });
+        load();
+      })
   })
 
-  // var data = [];
-  // var id = [];
-  // db.collection("kebun").orderBy("kode")
-  //   .onSnapshot((querySnapshot) => {
-  //     data = [];
-  //     querySnapshot.forEach((doc) => {
-  //       tempData = doc.data();
-  //       tempId = doc.id;
-  //       tempData['keys'] = tempId;
-  //       data.push(tempData);
-  //     });
-  //     load();
-  //   });
-
   function load() {
-    $("#spinner").remove();
     $("#jsGrid1").jsGrid({
       height: "100%",
       width: "100%",
@@ -253,7 +279,7 @@
         await db.collection("kebun").doc(args.previousItem.keys)
           .update(args.item)
           .then(function () {
-            console.log('Data Update ' + args.item.kode + ' Success');
+            console.log('Data Update ' + args.item.kapling + ' Success');
           }).catch(function (error) {
             console.log("Error updating document: ", error);
             alert('Data bermasalah');
@@ -264,11 +290,13 @@
       controller: {
         loadData: function(filter) {
           return $.grep(data, function(client) {
-            return (!filter.kode.toLowerCase() || client.kode.toLowerCase().indexOf(filter.kode.toLowerCase()) > -1)
-              && (!filter.nama.toLowerCase() || client.nama.toLowerCase().indexOf(filter.nama.toLowerCase()) > -1)
-              && (!filter.manager.toLowerCase() || client.manager.toLowerCase().indexOf(filter.manager.toLowerCase()) > -1)
-              && (!filter.kelompok.toLowerCase() || client.kelompok.toLowerCase().indexOf(filter.kelompok.toLowerCase()) > -1)
-              && (filter.master === undefined || client.master === filter.master);
+            return (!filter.kapling.toLowerCase() || client.kapling.toLowerCase().indexOf(filter.kapling.toLowerCase()) > -1)
+              && (!filter.kondisi.toLowerCase() || client.kondisi.toLowerCase().indexOf(filter.kondisi.toLowerCase()) > -1)
+              && (!filter.tipe.toLowerCase() || client.tipe.toLowerCase().indexOf(filter.tipe.toLowerCase()) > -1)
+              && (!filter.saran.toLowerCase() || client.saran.toLowerCase().indexOf(filter.saran.toLowerCase()) > -1)
+              && (!filter.pendapat.toLowerCase() || client.pendapat.toLowerCase().indexOf(filter.pendapat.toLowerCase()) > -1)
+              && (!filter.nama_petani.toLowerCase() || client.nama_petani.toLowerCase().indexOf(filter.nama_petani.toLowerCase()) > -1)
+              && (!filter.no_kontak.toLowerCase() || client.no_kontak.toLowerCase().indexOf(filter.no_kontak.toLowerCase()) > -1);
           });
         },
 
@@ -285,11 +313,17 @@
       data: data,
 
       fields: [
-        { name: "kode", title: "Kode", type: "text", width: 100, editing: false },
-        { name: "nama", title: "Nama", type: "text", width: 300, editing: false },
-        { name: "manager", title: "Manager", type: "text", width: 300, validate: "required" },
-        { name: "kelompok", title: "Group", type: "text", width: 100 },
-        { name: "master", title: "Show", type: "checkbox", width: 60 },
+        { name: "kode", title: "Kode", type: "text", width: 100, editing: false, visible: false },
+        { name: "kud", title: "Kode", type: "text", width: 100, editing: false, visible: false },
+        { name: "kt", title: "Kode", type: "text", width: 100, editing: false, visible: false },
+        { name: "tanggal", title: "Kode", type: "text", width: 100, editing: false, visible: false },
+        { name: "kapling", title: "Kapling", type: "text", width: 100 },
+        { name: "kondisi", title: "Kondisi Kapling saat kunjungan", type: "text", width: 170 },
+        { name: "tipe", title: "Type", type: "text", width: 40, validate: "required" },
+        { name: "saran", title: "Saran", type: "text", width: 120 },
+        { name: "pendapat", title: "Pendapat petani", type: "text", width: 120 },
+        { name: "nama_petani", title: "Nama Petani", type: "text", width: 100 },
+        { name: "no_kontak", title: "No Kontak", type: "text", width: 100 },
         { type: "control" , deleteButton: false }
       ]
     });
