c1fc657ea6754298a635880059945d4336a3c5ad
rainbowmeh <rainbowmeh@gmail.com>
2020-05-11 23:11:02 +0700

    update dashboard page

--- templates/menu.php
+++ templates/menu.php
@@ -1,8 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="../dashboard/index.php" class="brand-link">
-<!--    <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"-->
-<!--         style="opacity: .8">-->
     <span class="brand-text font-weight-light">Petani Plasma</span>
   </a>
 
@@ -10,9 +8,6 @@
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-<!--      <div class="image">-->
-<!--        <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">-->
-<!--      </div>-->
       <div class="info">
         <a href="#" class="d-block">Administrator</a>
       </div>
@@ -33,7 +28,7 @@
         </li>
         <li class="nav-item has-treeview">
           <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-users-cog"></i>
+            <i class="nav-icon fas fa-file-alt"></i>
             <p>
               Laporan
               <i class="right fas fa-angle-left"></i>
